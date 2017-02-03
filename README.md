9c1ca56c14b13d4aafa88015a8f7fd4993b8ceb1
Tony Ting <k999hoshing@gmail.com>
2017-02-03 23:06:47 +0800

    Use argparse. Add extension support. Edit README.

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # joe_notes_grabber
 Grab joe's notes
 Ha ha
+```sh
+usage: joe_notes_grabber.py [-h] --url url [-u username] [-p password]
+                            [-e [exts [exts ...]]]
+
+Hyperlink grabber
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --url url             Webpage to grab from
+  -u username, --user username
+                        Username for basic auth
+  -p password, --password password
+                        Password for basic auth
+  -e [exts [exts ...]], --extension [exts [exts ...]]
+                        File extension to grab. Default: .pdf .ppt .doc .docx
+```
--- joe_notes_grabber.py
+++ joe_notes_grabber.py
@@ -1,33 +1,39 @@
-from urllib.request import urlopen
-import urllib
-from bs4 import BeautifulSoup
-from bs4 import SoupStrainer
-import re
+#!/usr/bin/python3
 import os
+import urllib
+import re
 import base64
-
-user = "comp4017"
-passwd = "24001157"
-html_url = "http://cslinux0.comp.hkbu.edu.hk/~comp4017/2015-16/?page_id=8"
-grab_url = "http://cslinux0.comp.hkbu.edu.hk/~comp4017/2015-16/notes/"
+from argparse import ArgumentParser
+from urllib.request import urlopen
 
 
-def readURL(req):
-	return urlopen(req, timeout = 12).read()
+def grab(url, user="", passwd="", file_exts=[]):
+    auth_str = user + ":" + passwd
+    encoded_auth = base64.b64encode(auth_str.encode("utf8")).decode("utf8")
+    url_comps = urllib.parse.urlparse(url)
+    base_url = url_comps.scheme + "://" + url_comps.netloc + url_comps.path
+    html = urlopen(url, timeout=6).read().decode("utf-8")
+    # grab all hyper links
+    matches = re.findall(r'href=[\'"]?([^\'" >]+)', html)
 
-def main():
-	auth_str = user + ":" + passwd
-	encoded_auth = base64.b64encode(auth_str.encode("utf8")).decode("utf8")
-	html = readURL(html_url)
-	limit = SoupStrainer("td")
-	soup = BeautifulSoup(html, "html.parser", parse_only=limit)
-	pdfs = re.findall(r'href=[\'"]?([^\'" >]+)', soup.prettify())
-	for pdf in pdfs:
-		print("Grab " + os.path.basename(pdf))
-		r = urllib.request.Request(grab_url + os.path.basename(pdf))
-		if user != "" and passwd != "":
-			r.add_header("Authorization", "Basic " + encoded_auth)
-		open(os.path.basename(pdf), "wb").write(readURL(r))
+    for m in matches:
+        if os.path.splitext(m)[-1] in file_exts:
+            m_comps = urllib.parse.urlparse(m)
+            link = m if m_comps.scheme != "" else base_url + m_comps.path + m_comps.query
+            r = urllib.request.Request(link)
+            if user != "" and passwd != "":
+                r.add_header("Authorization", "Basic " + encoded_auth)
+            print("Grab " + os.path.basename(m))
+            print("    " + link)
+            open(os.path.basename(m), "wb").write(urlopen(r, timeout=6).read())
 
 if __name__ == "__main__":
-	main()
+    argparser = ArgumentParser(description="Hyperlink grabber")
+    argparser.add_argument("--url", metavar="url", type=str, required=True, help="Webpage to grab from")
+    argparser.add_argument("-u", "--user", type=str, metavar="username", default="", help="Username for basic auth")
+    argparser.add_argument("-p", "--password", type=str, metavar="password", default="", help="Password for basic auth")
+    argparser.add_argument("-e", "--extension", type=str, metavar="exts", nargs="*", default=[".pdf", ".ppt", ".doc", ".docx"], help="File extension to grab. Default: .pdf .ppt .doc .docx")
+
+    args = argparser.parse_args()
+
+    grab(url=args.url, user=args.user, passwd=args.password, file_exts=args.extension)
